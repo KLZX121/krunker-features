fe8933f7d5307914944b5e8d08b682e7b5b9228d
KLZX121 <kenneth121lzx@gmail.com>
2020-12-14 09:56:39 +1000

    Optisimisations

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 'use strict';
 
-//Menu Timer
+//move spectate button
+document.getElementById('spectButton').setAttribute('style', 'top: 20px;left: 550px');
+
+const menuTimer = document.createElement("div");
+menuTimer.setAttribute('id', 'menuTimer');
+menuTimer.setAttribute('style', "margin-bottom: 50px;color: cyan");
+
 setInterval(()=>{
 
-    //move spectateButton
-    document.getElementById('spectButton').setAttribute('style', 'top: 20px;left: 550px');
-
     //create element menuTimer
-    let menuTimer = document.createElement("div");
-    menuTimer.setAttribute('id', 'menuTimer');
-    menuTimer.setAttribute('style', "margin-bottom: 50px;color: cyan");
-
-    //append to instructions element
-    document.getElementById('instructions').appendChild(menuTimer);
-
+    if (!document.getElementById('menuTimer')) document.getElementById('instructions').appendChild(menuTimer);
+    
     //update menuTimer
     let time = document.getElementById('timerVal').innerHTML;
     if (!time || time == "00:00") time = document.getElementById('endTimer').innerHTML;
     document.getElementById('menuTimer').innerHTML = time;
     
-}, 1000);
+}, 500);
 
 //Find New Game on F4
 document.onkeydown = findNewGame;
