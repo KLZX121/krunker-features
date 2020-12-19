e9af0aef24f3c5be76b7a800d5e3665fdb204d18
KLZX121 <kenneth121lzx@gmail.com>
2020-12-19 23:58:38 +1000

    removed unneccesary code

--- content_scripts/winningTeam.js
+++ content_scripts/winningTeam.js
@@ -1,29 +1,11 @@
-const inGameUI = document.getElementById('inGameUI');
+'use strict';
 
-const observeGameUI = new MutationObserver((mutations, observer) => {
-    console.log(observer);
-    for (const mutation of mutations){
-
-        if (mutation.target.style.display === 'block'){
-            ffa()
-            observeGameUI.disconnect();
-        }
-    }
+const newScoreboard = document.getElementById('leaderContainerD'),
+    oldScoreboard = document.getElementById('leaderContainer');
+const observeLeaderboard = new MutationObserver(mutations => {
+    for(const mutation of mutations) {
+        console.log(mutation.addedNodes[0].childNodes[1].className) //newLeaderNameM or leaderNameM
+    };
 });
-
-observeGameUI.observe(inGameUI, {attributes: true});
-
-function ffa(){
-    //ffa
-    //const leaderboard = document.getElementById(`leaderContainer${ document.getElementById('leaderContainerD').innerHTML ? 'D' : ''}`)
-    /**/const newScoreboard = document.getElementById('leaderContainerD');
-    /**/const oldScoreboard = document.getElementById('leaderContainer');
-    const observeLeaderboard = new MutationObserver(mutations => {
-        for(const mutation of mutations) {
-            console.log(mutation.addedNodes[0].childNodes[1].className) //newLeaderNameM or leaderNameM
-        };
-    });
-    observeLeaderboard.observe(newScoreboard, { childList: true });
-    /**/observeLeaderboard.observe(oldScoreboard, { childList: true });
-}
-
+observeLeaderboard.observe(newScoreboard, { childList: true });
+observeLeaderboard.observe(oldScoreboard, { childList: true });
