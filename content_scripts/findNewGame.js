675e456f1af5bc4037e6744f56823d010cb28b21
KLZX121 <kenneth121lzx@gmail.com>
2020-12-19 23:58:21 +1000

    improved code in find new game for simplicity

--- content_scripts/findNewGame.js
+++ content_scripts/findNewGame.js
@@ -1,10 +1,9 @@
 'use strict';
 document.onkeydown = findNewGame;
-let hotkey;
 function findNewGame(a){
     chrome.storage.sync.get(['newGameHotkey'], result =>{
-        hotkey = result.newGameHotkey;
+        let hotkey = result.newGameHotkey;
+        if (a.key !== hotkey) return;
+        window.location.href = 'https://krunker.io';
     });
-    if (a.key !== hotkey) return;
-    window.location.href = 'https://krunker.io';
 }; 
