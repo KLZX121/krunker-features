38c8f38e34a76f98d1369a74d38e4b0d608acccc
KLZX121 <kenneth121lzx@gmail.com>
2020-12-22 18:05:03 +1000

    fixed up some timer issues

--- content_scripts/menuTimer.js
+++ content_scripts/menuTimer.js
@@ -11,16 +11,19 @@ const menuTimer = document.createElement("div");
 menuTimer.setAttribute('id', 'menuTimer');
 
 const timerVal = document.getElementById('timerVal');
-const observeTime = new MutationObserver(()=>{
+setInterval(()=>{
 
     let time = timerVal.innerHTML;
-    if (!document.getElementById('menuTimer')) document.getElementById('instructions').appendChild(menuTimer);
-
-    if (!time || time == "00:00") time = document.getElementById('endTimer').innerHTML;
+    if (!document.getElementById('menuTimer')) {
+        document.getElementById('instructions').appendChild(menuTimer);
+    }
+    console.log(time);
+    if (!time || time == "00:00") {
+        time = document.getElementById('endTimer').innerHTML;
+    }
     chrome.storage.sync.get('menuTimerColours', result => {
         menuTimer.style.color = parseInt(time) < 1 ? result.menuTimerColours[1] : result.menuTimerColours[0];
     });
     document.getElementById('menuTimer').innerHTML = time;
 
-})
-observeTime.observe(timerVal, {childList: true});
+}, 500);
