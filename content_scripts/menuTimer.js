bf7232d60f062a675a909c474c98247f74782de0
KLZX121 <kenneth121lzx@gmail.com>
2020-12-22 17:11:55 +1000

    doubled spectate button size

--- content_scripts/menuTimer.js
+++ content_scripts/menuTimer.js
@@ -1,7 +1,9 @@
 'use strict';
-document.getElementById('spectButton').setAttribute('style', 'top: 40%; ');
+const spectBtn = document.getElementById('spectButton');
+spectBtn.setAttribute('style', 'top: 35%; ');
+spectBtn.firstElementChild.setAttribute('style', 'font-size: 30px; color: rgba(255,255,255,0.6);');
 
-document.getElementById('spectButton').firstElementChild.setAttribute('style', 'font-size: 30px; color: rgba(255,255,255,0.6);');
+document.getElementsByClassName('switchsml')[1].setAttribute('style', 'bottom: 0.25em;')
 document.getElementsByClassName('sliderSml')[0].setAttribute('style', 'width: 43px; height: 30px; border-radius: 15px;');
 document.styleSheets[0].cssRules[466].style.cssText = "position: absolute; content: ''; height: 24px; width: 24px; left: 2px; bottom: 2px; background-color: rgb(255, 255, 255); transition: all 0.4s ease 0s; border-radius: 50%;";
 
