88febcbc827638981fba5be778f276ed1ddffce7
KLZX121 <kenneth121lzx@gmail.com>
2021-01-05 16:40:21 +1000

    changed spectate button to be correctly proportioned

--- content_scripts/menuTimer.js
+++ content_scripts/menuTimer.js
@@ -1,11 +1,6 @@
 'use strict';
 const spectBtn = document.getElementById('spectButton');
-spectBtn.setAttribute('style', 'top: 35%; ');
-spectBtn.firstElementChild.setAttribute('style', 'font-size: 30px; color: rgba(255,255,255,0.6);');
-
-document.getElementsByClassName('switchsml')[1].setAttribute('style', 'bottom: 0.25em;')
-document.getElementsByClassName('sliderSml')[0].setAttribute('style', 'width: 43px; height: 30px; border-radius: 15px;');
-document.styleSheets[0].cssRules[466].style.cssText = "position: absolute; content: ''; height: 24px; width: 24px; left: 2px; bottom: 2px; background-color: rgb(255, 255, 255); transition: all 0.4s ease 0s; border-radius: 50%;";
+spectBtn.style.top = 'calc(50% - 85px)';
 
 const menuTimer = document.createElement("div");
 menuTimer.setAttribute('id', 'menuTimer');
