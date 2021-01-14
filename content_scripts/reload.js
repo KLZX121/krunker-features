b09c212b8f6644ef8ceff3ab7bfad4210f492dc4
KLZX121 <kenneth121lzx@gmail.com>
2021-01-14 22:43:15 +1000

    reload now applies settings

--- content_scripts/reload.js
+++ content_scripts/reload.js
@@ -11,23 +11,32 @@ ammoBacking.append(g('ammoVal'), ' ', g('ammoMax'), g('ammoIcon'));
 const ammoDisplay = document.getElementById('ammoDisplay');
 document.getElementById('inGameUI').prepend(ammoDisplay);
 
-const ammoDisplayStyle = document.styleSheets[0].cssRules[232].style;
-ammoDisplayStyle.setProperty('background-image', 'linear-gradient(rgba(255,255,255,0.3), rgba(255,255,255,0.3))'); //reloading colour
-ammoDisplay.style = `position: absolute; background-color: transparent; width: 50px; height: 5px; top: calc(50% + 35px); left: calc(50% - 40px); border-radius: 0px;`;
+chrome.storage.sync.get(['reload', 'toggles'], result => {
+    const ammoDisplayStyle = document.styleSheets[0].cssRules[232].style;
+    const rgba = `rgba(${hexToRgb(result.reload.colour)},${result.reload.opacity})`;
+    console.log(rgba);
+    ammoDisplayStyle.setProperty('background-image', `linear-gradient(${rgba}, ${rgba})`); //reloading colour
+    
+    const left = result.reload.position[0].replace('-', '- ').replace('+', '+ ');
+    const top = result.reload.position[1].replace('-', '- ').replace('+', '+ ');
+    console.log(left);
+    console.log(top);
+    ammoDisplay.style = `position: absolute; background-color: transparent; 
+        ${result.toggles[4] ? '' : 'display: none;'}
+        width: ${result.reload.size[0]}; 
+        height: ${result.reload.size[1]}; 
+        left: calc(50% ${left});
+        top: calc(50% ${top}); 
+        border-radius: 0px;
+    `;
+    console.log(ammoDisplay.style);
+});
+
 
 function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? `rgb(${parseInt(result[1], 16)},${parseInt(result[2], 16)},${parseInt(result[3], 16)})` : null;
+    return result ? `${parseInt(result[1], 16)},${parseInt(result[2], 16)},${parseInt(result[3], 16)}` : null;
 }
-
-function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-};
-function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-};
-
 //credits to https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
 /*const a = new MutationObserver(mutations => {
     console.log(mutations[0]);
